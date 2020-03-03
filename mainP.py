09c961292916dcf37de99ec4ff8c3f1229942910
kevin luttrell <kevinluttrell@MacBook-Pro-6.local>
2020-03-03 17:51:40 -0600

    updated code

--- mainP.py
+++ mainP.py
@@ -1,56 +1,92 @@
-# Dependencies
-import os
 import csv
+import os
 
-# Calling CSV
-budget_data = "budget_data.csv"
+# Files to load and output (Remember to change these)
+file_to_load = os.path.join("Resources", "election_data.csv")
+file_to_output = os.path.join("analysis", "election_analysis.txt")
 
-# Creating dictionaries to store data
-months = []
+# Total Vote Counter
+total_votes = 0
 
-profitloss = []
+# Candidate Options and Vote Counters
+candidate_options = []
+candidate_votes = {}
 
-profitchange = []
+# Winning Candidate and Winning Count Tracker
+winning_candidate = ""
+winning_count = 0
 
-change_list = []
+# Read the csv and convert it into a list of dictionaries
+with open(file_to_load) as election_data:
+    reader = csv.reader(election_data)
 
-# Reading the CSV to read
-with open(budget_data, newline="") as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=",")
+    # Read the header
+    header = next(reader)
 
-    next(csvreader)
+    # For each row...
+    for row in reader:
 
-    for row in csvreader:
-        months.append(row[0])
-        profitloss.append(float(row[1]))
+        # Run the loader animation
+        print(". ", end=""),
 
-    for i in range(1, len(profitloss)):
-        profitchange.append(profitloss[i]-profitloss[i-1])
+        # Add to the total vote count
+        total_votes = total_votes + 1
 
-        changes = round(sum(profitchange)/len(profitchange), 2)
+        # Extract the candidate name from each row
+        candidate_name = row[2]
 
-        max_change = max(profitchange)
-        max_date = str(months[profitchange.index(max(profitchange))+ 1])
-        min_change = min(profitchange)
-        min_date = str(months[profitchange.index(min(profitchange))+ 1]) 
-    
-# printing out data in readable format
-print("Financial Analysis")
-print("--------------------------------")
-print(f"Total Months: {len(months)}")
-print(f"Total: ${int(sum(profitloss))}")
-print(f"Average Change: ${(changes)}")
-print(f"Greatest increase in profits: {max_date} ${int(max_change)}")
-print(f"Greatest decrease in profits: {min_date} ${int(min_change)}")
+        # If the candidate does not match any existing candidate...
+        # (In a way, our loop is "discovering" candidates as it goes)
+        if candidate_name not in candidate_options:
 
-fh = open("pybank.txt", "w")
+            # Add it to the list of candidates in the running
+            candidate_options.append(candidate_name)
 
-fh.write("Financial Analysis\n"
-"-------------------\n"
-f"Total Months: {len(months)}\n"
-f"Total: ${int(sum(profitloss))}\n"
-f"Average Change: ${(changes)}\n"
-f"Greatest increase in profits: {max_date} ${int(max_change)})\n"
-f"Greatest decrease in profits: {min_date} ${int(min_change)})")
+            # And begin tracking that candidate's voter count
+            candidate_votes[candidate_name] = 0
 
-fh.close()
+        # Then add a vote to that candidate's count
+        candidate_votes[candidate_name] = candidate_votes[candidate_name] + 1
+
+# Print the results and export the data to our text file
+with open(file_to_output, "w") as txt_file:
+
+    # Print the final vote count (to terminal)
+    election_results = (
+        f"\n\nElection Results\n"
+        f"-------------------------\n"
+        f"Total Votes: {total_votes}\n"
+        f"-------------------------\n")
+    print(election_results, end="")
+
+    # Save the final vote count to the text file
+    txt_file.write(election_results)
+
+    # Determine the winner by looping through the counts
+    for candidate in candidate_votes:
+
+        # Retrieve vote count and percentage
+        votes = candidate_votes.get(candidate)
+        vote_percentage = float(votes) / float(total_votes) * 100
+
+        # Determine winning vote count and candidate
+        if (votes > winning_count):
+            winning_count = votes
+            winning_candidate = candidate
+
+        # Print each candidate's voter count and percentage (to terminal)
+        voter_output = f"{candidate}: {vote_percentage:.3f}% ({votes})\n"
+        print(voter_output, end="")
+
+        # Save each candidate's voter count and percentage to text file
+        txt_file.write(voter_output)
+
+    # Print the winning candidate (to terminal)
+    winning_candidate_summary = (
+        f"-------------------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"-------------------------\n")
+    print(winning_candidate_summary)
+
+    # Save the winning candidate's name to the text file
+    txt_file.write(winning_candidate_summary)
