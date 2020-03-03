532bfb5b028be9893f303ebf34a5167b3a4f4520
kevin luttrell <kevinluttrell@MacBook-Pro-6.local>
2020-03-03 17:53:38 -0600

    updated code

--- mainP.py
+++ mainP.py
@@ -1,9 +1,8 @@
 import csv
 import os
 
-# Files to load and output (Remember to change these)
-file_to_load = os.path.join("Resources", "election_data.csv")
-file_to_output = os.path.join("analysis", "election_analysis.txt")
+# Files to load and output
+file_to_load = "election_data.csv"
 
 # Total Vote Counter
 total_votes = 0
