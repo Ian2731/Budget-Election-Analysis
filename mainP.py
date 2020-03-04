b22cfcbacdffbe277fb9bdf8b5a08ac912e1ef47
kevin luttrell <kevinluttrell@MacBook-Pro-6.local>
2020-03-03 18:07:38 -0600

    updated code

--- mainP.py
+++ mainP.py
@@ -1,91 +1,63 @@
-import csv
 import os
+import csv
 
-# Files to load and output
-file_to_load = "election_data.csv"
+pypoll = "election_data.csv"
 
-# Total Vote Counter
-total_votes = 0
+total = 0
 
-# Candidate Options and Vote Counters
-candidate_options = []
-candidate_votes = {}
+#Open and read csv
+with open(pypoll,newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
 
-# Winning Candidate and Winning Count Tracker
-winning_candidate = ""
-winning_count = 0
+    next(csvreader)
 
-# Read the csv and convert it into a list of dictionaries
-with open(file_to_load) as election_data:
-    reader = csv.reader(election_data)
+    voters = []
 
-    # Read the header
-    header = next(reader)
+    county = []
+    
+    candidate = []
 
-    # For each row...
-    for row in reader:
+    unique_candidate = []
 
-        # Run the loader animation
-        print(". ", end=""),
+    castvotes = []
+  
+    candidatedetail = []
 
-        # Add to the total vote count
-        total_votes = total_votes + 1
+    for row in csvreader:
+        
+        voters.append(row[0])
+        county.append(row[1])
+        candidate.append(row[2])
+    
+    print("Election Results")
+    print("--------------------------")
+    print(f"Total Votes: {len(voters)}")
+    print("--------------------------")
 
-        # Extract the candidate name from each row
-        candidate_name = row[2]
+    for x in candidate: 
+        # check if exists in unique_list or not 
+        if x not in unique_candidate: 
+            unique_candidate.append(x)
+            castvotes.append(candidate.count(x))
+            print(f"{(x)}: {round((candidate.count(x)/len(voters))*100,3)}% ({candidate.count(x)})")
+            candidatedetail.append(f"{(x)}: {round((candidate.count(x)/len(voters))*100,3)}% ({candidate.count(x)})")
 
-        # If the candidate does not match any existing candidate...
-        # (In a way, our loop is "discovering" candidates as it goes)
-        if candidate_name not in candidate_options:
+    max_winner = str(unique_candidate[castvotes.index(max(castvotes))])       
+    
+    print("--------------------------")
+    print(f"Winner: {max_winner}")
+    
+fh = open("pypoll.txt", "w")
 
-            # Add it to the list of candidates in the running
-            candidate_options.append(candidate_name)
+fh.write("Election Results\n"
+        "--------------------------\n"
+        f"Total Votes: {len(voters)}\n"
+        "--------------------------\n")
 
-            # And begin tracking that candidate's voter count
-            candidate_votes[candidate_name] = 0
-
-        # Then add a vote to that candidate's count
-        candidate_votes[candidate_name] = candidate_votes[candidate_name] + 1
-
-# Print the results and export the data to our text file
-with open(file_to_output, "w") as txt_file:
-
-    # Print the final vote count (to terminal)
-    election_results = (
-        f"\n\nElection Results\n"
-        f"-------------------------\n"
-        f"Total Votes: {total_votes}\n"
-        f"-------------------------\n")
-    print(election_results, end="")
-
-    # Save the final vote count to the text file
-    txt_file.write(election_results)
-
-    # Determine the winner by looping through the counts
-    for candidate in candidate_votes:
-
-        # Retrieve vote count and percentage
-        votes = candidate_votes.get(candidate)
-        vote_percentage = float(votes) / float(total_votes) * 100
-
-        # Determine winning vote count and candidate
-        if (votes > winning_count):
-            winning_count = votes
-            winning_candidate = candidate
-
-        # Print each candidate's voter count and percentage (to terminal)
-        voter_output = f"{candidate}: {vote_percentage:.3f}% ({votes})\n"
-        print(voter_output, end="")
-
-        # Save each candidate's voter count and percentage to text file
-        txt_file.write(voter_output)
-
-    # Print the winning candidate (to terminal)
-    winning_candidate_summary = (
-        f"-------------------------\n"
-        f"Winner: {winning_candidate}\n"
-        f"-------------------------\n")
-    print(winning_candidate_summary)
-
-    # Save the winning candidate's name to the text file
-    txt_file.write(winning_candidate_summary)
+for i in range(len(candidatedetail)):
+    fh.write(
+            f"{candidatedetail[i]}\n" 
+            )
+fh.write("--------------------------\n"
+        f"Winner: {max_winner}")
+fh.close()
